16997f22ad5ab63e8b364620435d910ea546d63b
LinasBeres <beres.linas@gmail.com>
2022-09-21 19:54:02 -0700

    GLFW doing something now

--- src/gui/window.cpp
+++ src/gui/window.cpp
@@ -18,18 +18,31 @@ Window::Window()
 
 int Window::RenderWindow(const std::string& scenePath)
 {
-	glfwInit();
+	const char* description;
+	if (!glfwInit())
+	{
+		glfwGetError(&description);
+		std::cerr << "Could not init due to: " << description << "\n Exiting.\n";
+		exit(EXIT_FAILURE);
+	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	window = glfwCreateWindow(renderGlobals.width, renderGlobals.height, "Spindulys", nullptr, nullptr);
 	if (!window)
 	{
-		std::cerr << "Could not create window - exiting...\n";
-		return 1;
+		glfwGetError(&description);
+		std::cerr << "Could not create window due to: " << description << "\n Exiting.\n";
+		glfwTerminate();
+		exit(EXIT_FAILURE);
 	}
 
 	glfwMakeContextCurrent(window);
