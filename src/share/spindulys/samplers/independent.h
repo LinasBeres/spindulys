f85ef8128311996b00eca0443974b91592cb96ec
LinasBeres <beres.linas@gmail.com>
2023-01-12 13:12:13 -0800

    Base file templating added

---
+++ src/share/spindulys/samplers/independent.h
@@ -0,0 +1,16 @@
+#ifndef SPINDULYS_SAMPLER_INDEPENDENT_H
+#define SPINDULYS_SAMPLER_INDEPENDENT_H
+
+#include "sampler.h"
+
+#include "../math/vec3.h"
+
+#include "../../spindulys.h"
+
+
+SPINDULYS_NAMESPACE_OPEN_SCOPE
+
+
+SPINDULYS_NAMESPACE_CLOSE_SCOPE
+
+#endif // SPINDULYS_SAMPLER_INDEPENDENT_H
---
+++ src/share/spindulys/samplers/sampler.h
@@ -0,0 +1,14 @@
+#ifndef SPINDULYS_SAMPLER_H
+#define SPINDULYS_SAMPLER_H
+
+#include "../math/vec3.h"
+
+#include "../../spindulys.h"
+
+
+SPINDULYS_NAMESPACE_OPEN_SCOPE
+
+
+SPINDULYS_NAMESPACE_CLOSE_SCOPE
+
+#endif // SAMPLER_H
